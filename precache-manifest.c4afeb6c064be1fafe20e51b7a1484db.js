397efc6999f37f10a5d3856144beb4005d9aadbe
Mikita Pilets <mykytapilec@gmail.com>
2020-06-24 10:01:54 +0300

    Updates

---
+++ precache-manifest.c4afeb6c064be1fafe20e51b7a1484db.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0e9bd413702a8c1f798c9545e14df1",
+    "url": "/nasa-pictures/index.html"
+  },
+  {
+    "revision": "f2356593c690b0d178df",
+    "url": "/nasa-pictures/static/css/14.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "f46b65cc92280f1eef4d",
+    "url": "/nasa-pictures/static/css/main.f8969983.chunk.css"
+  },
+  {
+    "revision": "f2356593c690b0d178df",
+    "url": "/nasa-pictures/static/js/14.c3eca03c.chunk.js"
+  },
+  {
+    "revision": "14af06ed03631046cf982050abf2838a",
+    "url": "/nasa-pictures/static/js/14.c3eca03c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f46b65cc92280f1eef4d",
+    "url": "/nasa-pictures/static/js/main.837918de.chunk.js"
+  },
+  {
+    "revision": "bd61eef0d34eb9198f0c",
+    "url": "/nasa-pictures/static/js/reactPlayerDailyMotion.9214ead1.chunk.js"
+  },
+  {
+    "revision": "37899808c560d286e945",
+    "url": "/nasa-pictures/static/js/reactPlayerFacebook.498e564f.chunk.js"
+  },
+  {
+    "revision": "5c0203ea218d0ec2eb33",
+    "url": "/nasa-pictures/static/js/reactPlayerFilePlayer.824175b5.chunk.js"
+  },
+  {
+    "revision": "02e2fd0fcb2450a47e69",
+    "url": "/nasa-pictures/static/js/reactPlayerMixcloud.d469820e.chunk.js"
+  },
+  {
+    "revision": "ba11c4ccd6805d12ecbc",
+    "url": "/nasa-pictures/static/js/reactPlayerPreview.c4f24471.chunk.js"
+  },
+  {
+    "revision": "7b1501ead62c8f1c069c",
+    "url": "/nasa-pictures/static/js/reactPlayerSoundCloud.e05e22ec.chunk.js"
+  },
+  {
+    "revision": "b467ff9cbe00e9d31c60",
+    "url": "/nasa-pictures/static/js/reactPlayerStreamable.6a57634b.chunk.js"
+  },
+  {
+    "revision": "261f8c4e8439323b02d6",
+    "url": "/nasa-pictures/static/js/reactPlayerTwitch.4ea934c0.chunk.js"
+  },
+  {
+    "revision": "4133f1f6714bd06baaa8",
+    "url": "/nasa-pictures/static/js/reactPlayerVidyard.65fba577.chunk.js"
+  },
+  {
+    "revision": "c5248c92845bc77c2366",
+    "url": "/nasa-pictures/static/js/reactPlayerVimeo.990149dd.chunk.js"
+  },
+  {
+    "revision": "ce27f48fb35c8bb404db",
+    "url": "/nasa-pictures/static/js/reactPlayerWistia.b1cb3f77.chunk.js"
+  },
+  {
+    "revision": "b09d8b37da793d076e57",
+    "url": "/nasa-pictures/static/js/reactPlayerYouTube.ca806fb1.chunk.js"
+  },
+  {
+    "revision": "ad1872471a7890eb0310",
+    "url": "/nasa-pictures/static/js/runtime-main.6b934b9e.js"
+  }
+]);
